e976186221963b427a7afc9be13c39b88b8c19ca
Huy <22699431.huy@student.iuh.edu.vn>
2026-09-09 12:36:22 +0700

    first commit

---
+++ src/main/java/iuh/fit/fe/be_websatlichkham/BeWebSatLichKhamApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.fe.be_websatlichkham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWebSatLichKhamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeWebSatLichKhamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/fit/fe/be_websatlichkham/BeWebSatLichKhamApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.fe.be_websatlichkham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeWebSatLichKhamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
